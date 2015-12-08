6597aefe34e01efdd6b2f557cac822d165b5a215
Jaln Rodger <jgr111@uclive.ac.nz>
2015-12-08 15:17:02 +1300

    House card interface that wasnt added to git. Whoops.
    -Jaln

---
+++ src/Model/Houses/HouseCards/HouseCard.java
@@ -0,0 +1,12 @@
+package Model.Houses.HouseCards;
+
+/**
+ * Created by jaln on 1/12/15.
+ */
+public interface HouseCard {
+    public int getCardBonus();
+    public void performImmediately();
+    public void performPreBattle();
+    public void performPostBattle();
+
+}
